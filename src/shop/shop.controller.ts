672032c8cec8fd2b9d1bc36014b470a82d223c47
Anuj <choudharyanuj17122gmail.com>
2024-08-26 01:18:19 +0530

    made necessary changes in routes according to the product_size entity

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -1,52 +1,36 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { createProductDto } from './dtos/create-product.dto';
-import { deleteProductDto } from './dtos/delete-product.dto';
-import { updateProductDto } from './dtos/update-product.dto';
-import { Product } from './entities/product.entity';
+import { CreateProductDto } from './dtos/create-product.dto';
+import { UpdateProductDto } from './dtos/update-product.dto';
 import { ShopService } from './services/shop.service';
+import { deleteProductDto } from './dtos/delete-product.dto';
 
 @Controller('shop')
 export class ShopController {
-    constructor(private shopService:ShopService){}
+  constructor(private readonly shopService: ShopService) {}
 
-    @Get()
-    async allItems(){
-        return await this.shopService.fetchproducts();
-    }
+  @Get()
+  async allItems() {
+    return await this.shopService.fetchProducts();
+  }
 
-    @Get(':id')
-    async oneItem(@Param('id') id:string){
-        return await this.shopService.fetchOneProduct(parseInt(id));
-    }
+  @Get(':id')
+  async oneItem(@Param('id') id: string) {
+    return await this.shopService.fetchOneProduct(parseInt(id));
+  }
 
-    @Post('/createproduct')
-    async createProduct(@Body() body: createProductDto){
-        this.shopService.createProduct(
-            body.name,
-            body.description,
-            body.category,
-            body.sku,
-            body.origin,
-            body.benefits,
-            body.uses, 
-            body.ingredients, 
-            body.safetyInformation, 
-            body.video1, 
-            body.video2 ,
-            body.price,
-            body.discountprice,
-            body.imageUrl,
-            body.bannerUrl);
-    }
+  @Post('/createproduct')
+  async createProduct(@Body() createProductDto: CreateProductDto) {
+    return await this.shopService.createProduct(createProductDto);
+  }
 
-    @Patch('updateproduct')
-    async updateProduct(@Body() body: Partial<Product> ) {
-        const id = body.id;
-        return await this.shopService.updateProduct(id,body);
-    }
+  @Patch('/updateproduct')
+  async updateProduct( @Body() updateProductDto: UpdateProductDto) {
+    const id = updateProductDto.id
+    return await this.shopService.updateProduct(id, updateProductDto);
+  }
 
-    @Delete('deleteproduct')
-    async deleteProduct(@Body() body:deleteProductDto){
-         return await this.shopService.deleteProduct(body.id);
-    }
+  @Delete('/deleteproduct')
+  async deleteProduct(@Body() deleteProductDto: deleteProductDto) {
+    return await this.shopService.deleteProduct(deleteProductDto.id);
+  }
 }
